63c93bf8381014f27c6d2507fe21055b26453bb5
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-03-05 12:54:15 +0100

    Ajout champs password

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,22 @@ function App() {
 			.required('Le champ est requis')
 			.min(2, 'Le champ doit contenir au moins deux caractères')
 			.max(10),
-		age: yup.number().typeError('Veuillez entrer un nombre').min(18),
+		age: yup
+			.number()
+			.typeError('Veuillez entrer un nombre')
+			.min(18, "L'âge doit être supérieur à 18 ans"),
+		password: yup
+			.string()
+			.required('Veuillez entrer un mot de passe')
+			.min(5, 'Le mot de passe est trop court')
+			.max(10, 'Le mot de passe est trop long'),
+		confirmPassword: yup
+			.string()
+			.required('Veuillez confirmer votre mot de passe')
+			.oneOf(
+				[yup.ref('password', '')],
+				'Les mots de passe doivent être identiques'
+			),
 	});
 
 	const {
@@ -49,6 +64,26 @@ function App() {
 					</label>
 					<input {...register('age')} id='age' type='number' />
 					{errors?.age && <p>{errors.age.message}</p>}
+				</div>
+				<div className='d-flex flex-column mb-20'>
+					<label htmlFor='password' className='mb-5'>
+						Mot de passe
+					</label>
+					<input {...register('password')} id='password' type='password' />
+					{errors?.password && <p>{errors.password.message}</p>}
+				</div>
+				<div className='d-flex flex-column mb-20'>
+					<label htmlFor='confirmPassword' className='mb-5'>
+						Confirmation mot de passe
+					</label>
+					<input
+						{...register('confirmPassword')}
+						id='confirmPassword'
+						type='password'
+					/>
+					{errors?.confirmPassword && (
+						<p>{errors.confirmPassword.message}</p>
+					)}
 				</div>
 				<button className='btn btn-primary'>Sauvegarder</button>
 			</form>
