07bb99ebf66a4c800b130a7b87c4b9eb471646d2
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-03-05 14:52:14 +0100

    Ajout checkbox, radio & select

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,32 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 function App() {
 	const yupSchema = yup.object({
-		name: yup
-			.string()
-			.required('Le champ est requis')
-			.min(2, 'Le champ doit contenir au moins deux caractères')
-			.test('isYes', "L'API a dit non", async () => {
-				const response = await fetch('https://yesno.wtf/api');
-				const result = await response.json();
-				console.log(result);
-				return result.answer === 'yes';
-			}),
-		age: yup
-			.number()
-			.typeError('Veuillez entrer un nombre')
-			.min(18, "L'âge doit être supérieur à 18 ans"),
-		password: yup
-			.string()
-			.required('Veuillez entrer un mot de passe')
-			.min(5, 'Le mot de passe est trop court')
-			.max(10, 'Le mot de passe est trop long'),
-		confirmPassword: yup
-			.string()
-			.required('Veuillez confirmer votre mot de passe')
-			.oneOf(
-				[yup.ref('password', '')],
-				'Les mots de passe doivent être identiques'
-			),
+		// name: yup
+		// 	.string()
+		// 	.required('Le champ est requis')
+		// 	.min(2, 'Le champ doit contenir au moins deux caractères')
+		// 	.test('isYes', "L'API a dit non", async () => {
+		// 		const response = await fetch('https://yesno.wtf/api');
+		// 		const result = await response.json();
+		// 		console.log(result);
+		// 		return result.answer === 'yes';
+		// 	}),
+		// age: yup
+		// 	.number()
+		// 	.typeError('Veuillez entrer un nombre')
+		// 	.min(18, "L'âge doit être supérieur à 18 ans"),
+		// password: yup
+		// 	.string()
+		// 	.required('Veuillez entrer un mot de passe')
+		// 	.min(5, 'Le mot de passe est trop court')
+		// 	.max(10, 'Le mot de passe est trop long'),
+		// confirmPassword: yup
+		// 	.string()
+		// 	.required('Veuillez confirmer votre mot de passe')
+		// 	.oneOf(
+		// 		[yup.ref('password', '')],
+		// 		'Les mots de passe doivent être identiques'
+		// 	),
 	});
 
 	const {
@@ -39,7 +39,13 @@ function App() {
 		//watch,
 		formState: { errors },
 	} = useForm({
-		defaultValues: { name: '' },
+		defaultValues: {
+			name: '',
+			other: {
+				astro: '',
+				happy: false,
+			},
+		},
 		resolver: yupResolver(yupSchema),
 		mode: 'onBlur',
 	});
@@ -63,6 +69,7 @@ function App() {
 					<input {...register('name')} id='name' type='text' />
 					{errors?.name && <p>{errors.name.message}</p>}
 				</div>
+
 				<div className='d-flex flex-column mb-20'>
 					<label htmlFor='age' className='mb-5'>
 						Age
@@ -70,6 +77,57 @@ function App() {
 					<input {...register('age')} id='age' type='number' />
 					{errors?.age && <p>{errors.age.message}</p>}
 				</div>
+
+				<div className='d-flex flex-column mb-20'>
+					<label htmlFor='sexe' className='mb-5'>
+						Sexe
+					</label>
+					<div>
+						<label htmlFor='man'>Homme</label>
+						<input
+							{...register('gender')}
+							id='man'
+							value='man'
+							type='radio'
+						/>
+					</div>
+					<div>
+						<label htmlFor='woman'>Femme</label>
+						<input
+							{...register('gender')}
+							id='woman'
+							value='woman'
+							type='radio'
+						/>
+					</div>
+				</div>
+
+				<div className='d-flex flex-column mb-20'>
+					<label htmlFor='happy' className='mb-5'>
+						Content
+						<input
+							{...register('other.happy')}
+							id='happy'
+							type='checkbox'
+						/>
+					</label>
+				</div>
+
+				<div className='d-flex flex-column mb-20'>
+					<label htmlFor='astro' className='mb-5'>
+						Signe astrologique
+					</label>
+					<select {...register('other.astro')} id='astro'>
+						<option value='' disabled>
+							Sélectionnez votre signe
+						</option>
+						<option value='fish'>Poisson</option>
+						<option value='aquarius'>Verseau</option>
+						<option value='taurus'>Taureau</option>
+						<option value='lion'>Lion</option>
+					</select>
+				</div>
+
 				<div className='d-flex flex-column mb-20'>
 					<label htmlFor='password' className='mb-5'>
 						Mot de passe
@@ -77,6 +135,7 @@ function App() {
 					<input {...register('password')} id='password' type='password' />
 					{errors?.password && <p>{errors.password.message}</p>}
 				</div>
+
 				<div className='d-flex flex-column mb-20'>
 					<label htmlFor='confirmPassword' className='mb-5'>
 						Confirmation mot de passe
@@ -90,6 +149,7 @@ function App() {
 						<p>{errors.confirmPassword.message}</p>
 					)}
 				</div>
+
 				<button className='btn btn-primary'>Sauvegarder</button>
 			</form>
 		</div>
