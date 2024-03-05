bcd484a2ea003d6dd603e9fda557b572b05617e1
Faustine Brachotte <faustine.brachotte@gmail.com>
2024-03-05 11:47:43 +0100

    Ajout react-hook-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useForm } from 'react-hook-form';
+
 function App() {
+	const { register, handleSubmit, getValues, watch } = useForm();
+
+	watch();
+
+	function submit(values) {
+		console.log(values);
+	}
+
+	console.log(getValues());
+
 	return (
-		<>
-			<h1>Hello</h1>
-		</>
+		<div
+			className='d-flex flex-row justify-content align-items'
+			style={{ backgroundColor: '#fefefe', height: '100vh', width: '100%' }}
+		>
+			<form onSubmit={handleSubmit(submit)}>
+				<div className='d-flex flex-column mb-20'>
+					<label htmlFor='name' className='mb-5'>
+						Nom
+					</label>
+					<input {...register('name')} id='name' type='text' />
+				</div>
+				<button className='btn btn-primary'>Sauvegarder</button>
+			</form>
+		</div>
 	);
 }
 
